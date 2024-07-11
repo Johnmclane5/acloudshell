0ebec555baba772bd5c16a2b6eb3765e11026055
Johnmclane5 <134460067+Johnmclane5@users.noreply.github.com>
2024-07-11 09:39:36 +0000

    Pending changes exported from your codespace

---
+++ bot/helper/ext_utils/aeon_utils.py
@@ -0,0 +1,13 @@
+import re
+import pyshorteners
+from bot import LOGGER
+
+def tinyfy(long_url):
+    s = pyshorteners.Shortener()
+    try:
+        short_url = s.clckru.short(long_url)
+        LOGGER.info(f'tinyfied {long_url} to {short_url}')
+        return short_url
+    except Exception:
+        LOGGER.error(f'Failed to shorten URL: {long_url}')
+        return long_url
